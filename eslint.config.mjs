aeaa006aaea700fc3f6f15e870c885e609c77ffb
roypr <me.partha04@gmail.com>
2026-06-20 17:43:36 +0530

    Visual improvements

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,27 @@
 import js from "@eslint/js";
+import css from "@eslint/css";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
+  { 
+    files: ["**/*.{js,mjs,cjs}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { globals: globals.browser } 
+  },
+  { 
+    files: ["**/*.js"], 
+    languageOptions: { sourceType: "module" } 
+  },
+  {
+    files: ["**/*.css"],
+    plugins: {
+      css,
+    },
+    language: "css/css",
+    rules: {
+      "css/no-duplicate-imports": "error",
+    },
+  },
 ]);
